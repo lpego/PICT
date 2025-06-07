7c5a1aea89c0cab88799d8ac57fb7816c8824910
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-06-07 18:25:30 +0200

    spaces cause problems? Since when?

--- start_video_v2.0.sh
+++ start_video_v2.0.sh
@@ -5,5 +5,5 @@ sudo killall python
 
 # Start Python script for video recording and redirect output (STD.OUT & STD.ERR)
 # must be in the same directory as the .py script
-time = `date +"%Y%m%d_%H%M"`
+time=`date +"%Y%m%d_%H%M"`
 nohup python videos_v2.0.py > log_videos_${time}.out 2>&1 & 
