3193eebe99c987f2cde65ad767bd18554da87625
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-06-09 16:25:04 +0200

    Adding new python script and launcher for autofocus recording

---
+++ start_video_v3.0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# # Ensure that no other processes are using the camera
+# sudo killall python || true
+
+# Start Python script for video recording and redirect output (STD.OUT & STD.ERR)
+# must be in the same directory as the .py script
+time=`date +"%Y%m%d_%H%M"`
+nohup python videos_v3.0.py > log_videos_${time}.out 2>&1 &
