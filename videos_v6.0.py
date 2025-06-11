566105f7f4c45e6a43d8b3cde1b0a95e2d472be7
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-06-11 15:43:35 +0200

    re-implementing autofocus toggle, cleaning up a bit too

--- videos_v6.0.py
+++ videos_v6.0.py
@@ -5,17 +5,18 @@ import socket
 import uuid
 import os
 import cv2
-
-from picamera2 import MappedArray, Picamera2
-from picamera2.encoders import H264Encoder, Quality
-from picamera2 import controls
 from picamera2 import Picamera2
+from picamera2 import MappedArray
+from picamera2.encoders import H264Encoder, Quality
 from libcamera import controls
 
 ### Parameters declaration
 video_duration = 10
 video_number = 6
+resolution = (1296, 972)  # Set the desired resolution as (width, height)
 target_fps = 10
+focus = "Auto"  # Set to "Auto" or "Manual"
+focus_distance = 30.0  # Only used if Focus is "Manual"; must be between 0 and 1024
 video_dir = "/home/pi/record/videos/"
 os.makedirs(video_dir, exist_ok=True)
 
@@ -24,22 +25,20 @@ HostName = socket.gethostname()
 
 ### Initialize Picamera2 custom config
 picam2 = Picamera2()
+
+if focus == "Auto":
+    picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+elif focus == "Manual":
+    lens_position = focus_distance if 0 <= focus_distance <= 1024 else 30.0
+    picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": lens_position})
+else:
+    raise ValueError("Focus must be either 'Auto' or 'Manual'")
+
 picam2.video_configuration.controls.FrameRate = target_fps
-picam2.video_configuration.main.size = (1296, 972)
+picam2.video_configuration.main.size = resolution
 picam2.video_configuration.main.format = "YUV420"
-picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-# picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0})
+picam2.video_configuration.align()
 picam2.configure("video")
-
-
-###
-# print(picam2.camera_configuration()['sensor'])
-# print(picam2.camera_configuration()['raw'])
-
-
-# ### Basic config for testing
-# picam2 = Picamera2()
-# picam2.configure(picam2.create_video_configuration())
 
 ### Overlay parameters
 colour = (0, 255, 0)
@@ -57,7 +56,7 @@ def apply_timestamp(request):
 picam2.pre_callback = apply_timestamp
 
 ### Initialise encoder, w/ bitrate
-encoder = H264Encoder(10000000)
+encoder = H264Encoder()
 
 ### Signal handling
 running = True
@@ -75,18 +74,12 @@ for h in range(video_number):
 
     ### Start camera with custom config and overlay text
     picam2.start()
-    picam2.set_controls({"FrameRate": target_fps})
     start = time.time()
     picam2.start_recording(encoder, f"{video_dir}OverlayTest_{HostName}_{UID}_{h+1:03d}.h264", quality=Quality.HIGH)
     while (time.time() - start) < video_duration and running:
         time.sleep(0.1)
     
     picam2.stop_recording()
-    
-    # ### Basic event loop for testing
-    # picam2.start_recording(encoder, f"{video_dir}OverlayTest_{HostName}_{UID}_{h+1:03d}.h264")
-    # time.sleep(5)
-    # picam2.stop_recording()
 
     print(f"Finished recording {filename}")
 
