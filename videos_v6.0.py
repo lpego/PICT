f46000755cf092be02db43ff235dde8fa2ed1594
Luca Pegoraro <luca.pegoraro90@gmail.com>
2025-06-11 11:46:37 +0200

    Attempting to use custom configs and event loop

--- videos_v6.0.py
+++ videos_v6.0.py
@@ -7,7 +7,7 @@ import os
 import cv2
 
 from picamera2 import MappedArray, Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import H264Encoder, Quality
 
 ### Parameters declaration
 video_duration = 1800
@@ -19,19 +19,23 @@ os.makedirs(video_dir, exist_ok=True)
 UID = datetime.now().strftime('%Y-%m-%d_%H-%M') + '_' + uuid.uuid4().hex[:4].upper()
 HostName = socket.gethostname()
 
-### Initialize Picamera2
-# # picam2 = Picamera2()
-# config = picam2.create_video_configuration(
-#     main={"size": (1296, 972), "format": "YUV420"},
-#     controls={
-#         "FrameRate": target_fps,
-#         "AfMode": controls.AfModeEnum.Continuous
-#     }
-# )
-# picam2.configure(config)
-
+### Initialize Picamera2 custom config
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
+config = picam2.create_video_configuration(
+    main={
+        "size": (1296, 972),
+        # "format": "YUV420"
+        },
+    controls={
+        "FrameRate": target_fps,
+        "AfMode": controls.AfModeEnum.Continuous
+    }
+)
+picam2.configure(config)
+
+# ### Basic config for testing
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_video_configuration())
 
 ### Overlay parameters
 colour = (0, 255, 0)
@@ -45,7 +49,6 @@ def apply_timestamp(request):
     timestamp = time.strftime("%Y-%m-%d %X")
     with MappedArray(request, "main") as m:
         cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
-
 
 picam2.pre_callback = apply_timestamp
 
@@ -66,26 +69,27 @@ for h in range(video_number):
     filename = f"{video_dir}{HostName}_{UID}_{h+1:03d}.h264"
     print(f"Recording {filename}")
 
-    # # Start camera with overlay text
-    # picam2.start()
-    # picam2.set_controls({"FrameRate": target_fps})
-    # # picam2.start_recording(filename)
-    # picam2.start_recording(encoder, filename, quality=Quality.HIGH)
+    ### Start camera with custom config and overlay text
+    picam2.start()
+    picam2.set_controls({"FrameRate": target_fps})
+    # picam2.start_recording(filename)
+    picam2.start_recording(encoder, f"{video_dir}OverlayTest_{HostName}_{UID}_{h+1:03d}.h264", quality=Quality.HIGH)
 
-    # start = time.time()
+    start = time.time()
 
-    # while time.time() - start < video_duration and running:
-    #     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    #     overlay_text = f"{HostName}, {target_fps} fps, {timestamp}"
-    #     picam2.set_overlay(overlay_text)
-    #     time.sleep(1)  # Update every second
+    while time.time() - start < video_duration and running:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        overlay_text = f"{HostName}, {target_fps} fps, {timestamp}"
+        picam2.set_overlay(overlay_text)
+        time.sleep(1)  # Update every second
 
-    # picam2.stop_recording()
-    # picam2.stop()
-    
-    picam2.start_recording(encoder, f"{video_dir}OverlayTest_{HostName}_{UID}_{h+1:03d}.h264")
-    time.sleep(5)
     picam2.stop_recording()
+    picam2.stop()
+    
+    # ### Basic event loop for testing
+    # picam2.start_recording(encoder, f"{video_dir}OverlayTest_{HostName}_{UID}_{h+1:03d}.h264")
+    # time.sleep(5)
+    # picam2.stop_recording()
 
     print(f"Finished recording {filename}")
 
