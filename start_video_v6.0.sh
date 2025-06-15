978d72492f46e876a2bcdda07811e9c3db2cbad3
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-06-15 11:05:00 +0200

    making dir for logs if it does not exist yet

--- start_video_v6.0.sh
+++ start_video_v6.0.sh
@@ -6,4 +6,5 @@ sudo systemctl stop live-preview.service
 # Start Python script for video recording and redirect output (STD.OUT & STD.ERR)
 # must be in the same directory as the .py script
 time=`date +"%Y%m%d_%H%M"`
+mkdir -p record
 nohup python videos_v6.0.py > record/log_videos_${time}.out 2>&1 &
