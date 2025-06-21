56791ba647d0e5f66ad3a91bb0d6e6a4ad01396b
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-06-21 16:55:57 +0200

    Wide format, manual focus

---
+++ start_video_v6.2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Lock file to prevent multiple instances
+LOCKFILE="/tmp/recording.lock"
+if [ -f "$LOCKFILE" ]; then
+    echo "[INFO] Recording already in progress. Skipping."
+    exit 0
+fi
+touch "$LOCKFILE"
+trap "rm -f $LOCKFILE" EXIT
+
+# # Close preview server
+sudo systemctl stop live-preview.service
+
+# Start Python script for video recording and redirect output (STD.OUT & STD.ERR)
+# must be in the same directory as the .py script
+time=`date +"%Y%m%d_%H%M"`
+mkdir -p record
+nohup python videos_v6.2.py > record/log_videos_${time}.out 2>&1 &
