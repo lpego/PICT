5bec95694c52537522066cd4a4fef248e3c27934
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-06-27 12:08:33 +0200

    actually reading lens position

--- metadata_test.py
+++ metadata_test.py
@@ -6,3 +6,4 @@ picam2.start()
 for i in range(30):
     metadata = picam2.capture_metadata()
     print("Frame", i, "has arrived")
+    print(f"Current Lens Position is: {metadata.get('LensPosition', 'N/A'):.2f}")
