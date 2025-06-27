3be7e581ccdf4a6f8e7bb3fd2f436c322231498c
Luca Pegoraro <luca.pegoraro@wsl.ch>
2025-06-27 17:50:18 +0200

    Deleting obsolete versions of video recording scripts

--- start_video_v2.0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# # Ensure that no other processes are using the camera
-# sudo killall python || true
-
-# Start Python script for video recording and redirect output (STD.OUT & STD.ERR)
-# must be in the same directory as the .py script
-time=`date +"%Y%m%d_%H%M"`
-nohup python videos_v2.0.py > log_videos_${time}.out 2>&1 &
